9266a510b851bd6d8125f0ad56de0c7369f9712b
snigdhardy <163920844+snigdhardy@users.noreply.github.com>
2024-07-07 21:38:56 +0530

    Add files via upload
    I've made a basic website on cricket,my favourite game.Although I've used very basic JS,I'll improve upon this and add new features.I'm planning to make a IPL 2024 website of my favourite team RCB❤️.

---
+++ script.js
@@ -0,0 +1,9 @@
+function Virat(){
+    document.getElementById('myImage').src='image1.png';
+}
+function Dhoni(){
+    document.getElementById('myImage').src='image3.png';
+}
+function Rohith(){
+    document.getElementById('myImage').src='image2.png';
+}
